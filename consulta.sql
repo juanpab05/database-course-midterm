a3c1ec8ed9432cffe510e2127a7f62e41cf07be9
juanpab05 <165406198+juanpab05@users.noreply.github.com>
2024-12-03 20:33:13 -0500

    Update consulta.sql

--- consulta.sql
+++ consulta.sql
@@ -1,1 +1,5 @@
-SELECT id_ruta, placa, fecha, (SELECT capacidad FROM vehiculo WHERE vehiculoruta.placa = vehiculo.placa) FROM vehiculoruta
+SELECT fecha, id_ruta, 
+(SELECT nombre AS nombre_ruta FROM ruta WHERE vehiculoruta.id_ruta = ruta.id_ruta), 
+placa,
+(SELECT capacidad FROM vehiculo WHERE vehiculoruta.placa = vehiculo.placa)
+FROM vehiculoruta 
